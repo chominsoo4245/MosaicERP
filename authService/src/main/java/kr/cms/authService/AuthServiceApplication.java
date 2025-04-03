1f1182e8b38fe7fadac36d8a337de1eeda7261e3
chominsoo4245 <chominsoo4245@gmail.com>
2025-04-03 11:34:01 +0900

    database hibernate 기반, JSP 기반 으로 임시 통신 - AuthService 추가 예정 - - Postgresql, Oracle, MySQL - - Secret Info to *.env 파일로 추가 예정
    next step -> UserService, Gateway (Spring Cloud)

---
+++ authService/src/main/java/kr/cms/authService/AuthServiceApplication.java
@@ -0,0 +1,19 @@
+package kr.cms.authService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EntityScan(basePackages = "kr.cms.authService.domain")
+@EnableJpaRepositories(basePackages = "kr.cms.authService.repository")
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
